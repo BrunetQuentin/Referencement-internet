1821a739a1de73c536989972a1ba6565d25c5ac5
user1 <pranierb@gmail.com>
2023-01-17 11:47:49 +0100

    Conformisation des pages pour publier les missions et les CV conformément au rouoteur

--- php/publier_mission.php
+++ php/publier_mission.php
@@ -1,11 +1,2 @@
-<html lang="fr">
-<head>
-	<meta charset="UTF-8">
-	<meta name="description" content="Publier sa mission en tant qu'entreprise">
-	<title>Publier sa mission</title>
-</head>
-<body>
-	<main>
-	</main>
-</body>
-</html>
+<main>
+</main>
