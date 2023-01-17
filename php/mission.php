6ff9293bd1613f3c23c97813ebdd65b10ae6f68e
user1 <pranierb@gmail.com>
2023-01-17 11:54:07 +0100

    Création de la page des missions

---
+++ php/mission.php
@@ -0,0 +1,9 @@
+<main>
+	<h1>Annonces pour <strong>Freelance</strong></h1>
+
+	<p>Sur cette page, en tant qu'entreprise vous pourrez demander soutient à un particulier ou plusieurs, ce site étant spécialiste en intermédiaire freelance, vous pourrez recruter plein de codeurs pour satisfaire les besoins de vos missions, qu'elles soient des applications web, des services d'applications, des applications mobiles ou des sites web.</p>
+
+	<p>N'oubliez pas dans le cas d'un site Web de dire son CMS, la structure de son Back et du Front ou bien encore les mises en place SEO.</p>
+
+	<p>Il est important de définir le support du projet, par exemple si il est sous Android il ne seras probablement pas fait de la même manière que si il est sous iPhone à moins de faire du développement hybride, comme sa peut être le cas avec Windows, Apple et Linux.</p>
+</main>
