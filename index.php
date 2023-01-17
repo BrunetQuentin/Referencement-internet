c75215f245ef5868f9b12aaf4315c7c6486aad0c
quentin <brunetq38@gmail.com>
2023-01-17 10:42:00 +0100

    Structure projet

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$implementedPages = ['index'];
+$implementedPages = ['guide'];
 // Include the requested html page
 
 // get the url path
@@ -7,9 +7,10 @@ $pathArr = explode('/', $_SERVER['REQUEST_URI']);
 array_shift($pathArr);
 $path = join('/', $pathArr);
 
+// if the path is not in the implemented pages, redirect to error page
 if (!in_array($path, $implementedPages)) {
     $path = 'error';
 }
 
-include $path . '.html';
+include 'php/' . $path . '.php';
 ?>
