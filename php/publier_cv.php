0515fbbcbfd66e77378fcc9499dbe7a3c77d4b83
user1 <pranierb@gmail.com>
2023-01-17 11:17:09 +0100

    SEO de publier_cv.php

---
+++ php/publier_cv.php
@@ -0,0 +1,59 @@
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Publier son CV en tant que freelance">
+	<title>Publier son CV</title>
+</head>
+<body>
+	<main>
+		Sur cette page, en tant que <strong>développeur freelance</strong>, vous pourrez <strong>publier votre CV</strong> pour que notre système de pub puisse vous <strong>mettre en relation avec des entreprises</strong> pour de potentiels emplois dans différents projets informatiques.
+
+		<p>Exemple de languages :<p>
+		<ul>
+			<li>Java</li>
+			<li>NodeJs</li>
+			<li>Typescript</li>
+			<li>Flutter</li>
+			<li>React Natif</li>
+			<li>Angular</li>
+			<li>C++</li>
+			<li>JavaScript</li>
+			<li>Python</li>
+			<li>CSS</li>
+			<li>Html</li>
+			<li>Vue</li>
+			<li>Ruby</li>
+			<li>SQL</li>
+			<li>PHP</li>
+			<li>MongoDb</li>
+			<li>Django</li>
+			<li>Oracle</li>
+			<li>Test</li>
+			<li>Full Stack</li>
+			<li>C#</li>
+			<li>ReactJs</li>
+			<li>Perl</li>
+			<li>Kotlin</li>
+		</ul>
+
+		<p>Exemple de Mode de Développement :</p>
+		<ul>
+			<li>Agile</li>
+			<li>Semi-agile</li>
+			<li>Intégration Continue</li>
+			<li>SCRUM</li>
+		</ul>
+
+		<p><strong>Toute les expériences sont acceptés</strong>, voici quelques exemples :</p>
+		<ul>
+			<li>Ingénieur</li>
+			<li>Bac +3</li>
+			<li>Master</li>
+			<li>Spécialiste</li>
+			<li>Bac +2</li>
+			<li>Junior</li>
+			<li>Senior</li>
+		</ul>
+	</main>
+</body>
+</html>
