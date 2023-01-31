dd6c2f86a2215fae3d11cbede55b1ad91fbebcd3
quentin <brunetq38@gmail.com>
2023-01-31 10:47:37 +0100

    Solve url problem

--- php/publier_mission.php
+++ php/publier_mission.php
@@ -2,5 +2,16 @@
 <main>
     <h1>Publier toutes vos <strong>missions</strong> sur cette page</h1>
 
+    <p>Sur cette page, en tant qu'entreprise votre travail est de rendre vos missions le plus attractifs possible !</p>
+    <p>Vous pouvez définir vos offres ainsi que vos tarifs ou salaires</p>
+    <p>Expliquer de maniére claire votre eprojet grace a notre outil de création</p>
+    <p>Ainsi recruter de développeurs devient un jeu d'enfant !</p>
 
+    <p>Exemple de raison de recruter un développeur:</p>
+    <ol>
+        <li>Améliorer vos projets</li>
+        <li>Réaliser une application mobile</li>
+        <li>Réaliser une application Windows</li>
+        <li></li>
+    </ol>
 </main>
