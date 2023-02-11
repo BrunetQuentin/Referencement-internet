e327002a2af888c6622a99443b03f1d99404b923
quentin <brunetq38@gmail.com>
2023-02-11 14:46:49 +0100

    Ajout images

--- guide.php
+++ guide.php
@@ -1,30 +1,34 @@
-<?php include 'infodoc.php' ?>
+<?php include 'infodoc.php'; ?>
 <html lang="fr">
-<?php include 'head.php' ?>
+<?php include 'head.php'; ?>
 <body>
-	<?php include 'nav.php' ?>
-    <main>
-        <h1>Le guide complet pour utiliser <strong>AlpesFreelance</strong></h1>
-        <p>Bienvenue sur notre site de référencement pour développeur freelance. 
-            Ici vous pouvez vous mettre en contact avec des entreprise ou des particulier et gratuitement ! 
-            Ce site fournit en effet gratuitement sont service, le site sert d'intermédiaire pour les transactions d'argent et les annonces pour freelance et entreprises. 
-            Vous pourrez également augmenter votre visibilité par des actions telles que :</p>
-            <ul>
-                <li><a href="/publier_cv">Publier votre CV</a></li>
-                <li><a href="/publier_mission">Publier votre mission</a></li>
-            </ul>
-
-        <h2>Vous êtes un freelance:</h2>
-        <p>L'onglet "publier son cv" ainsi que "missions" sont fait pour vous !</p>
-        <p>En effet grace a ce site, il est possible très facilement de créer votre CV et de le publier.</p>
-        <p>De plus une multitude de mission  vous sont proposer.</p>
-
-        <br/>
-
-        <h2>Vous êtes une entreprise:</h2>
-        <p>L'onglet "publier sa mission" ainsi que "Freelance" sont fait pour vous !</p>
-        <p>En effet grace a ce site, il est possible très facilement de créer votre page de mission et de le publier.</p>
-        <p>De plus une multitude de freelance vous sont proposer.</p>
+	<?php include 'nav.php'; ?>
+    <main class="divide-2">
+        <div>
+            <h1>Le guide complet pour utiliser <strong>AlpesFreelance</strong></h1>
+            <p>Bienvenue sur notre site de référencement pour développeur freelance. 
+                Ici vous pouvez vous mettre en contact avec des entreprise ou des particulier et gratuitement ! 
+                Ce site fournit en effet gratuitement sont service, le site sert d'intermédiaire pour les transactions d'argent et les annonces pour freelance et entreprises. 
+                Vous pourrez également augmenter votre visibilité par des actions telles que :</p>
+                <ul>
+                    <li><a href="/publier_cv">Publier votre CV</a></li>
+                    <li><a href="/publier_mission">Publier votre mission</a></li>
+                </ul>
+    
+            <h2>Vous êtes un freelance:</h2>
+            <p>L'onglet "publier son cv" ainsi que "missions" sont fait pour vous !</p>
+            <p>En effet grace a ce site, il est possible très facilement de créer votre CV et de le publier.</p>
+            <p>De plus une multitude de mission  vous sont proposer.</p>
+    
+            <br/>
+    
+            <h2>Vous êtes une entreprise:</h2>
+            <p>L'onglet "publier sa mission" ainsi que "Freelance" sont fait pour vous !</p>
+            <p>En effet grace a ce site, il est possible très facilement de créer votre page de mission et de le publier.</p>
+            <p>De plus une multitude de freelance vous sont proposer.</p>
+            
+        </div>
+        <img src="images/guide.png"/>
     </main>
 </body>
 </html>
